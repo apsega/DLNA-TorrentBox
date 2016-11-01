6effeeb9cadd7a452a432daea3f5467c759df2dc
Edgaras <edgaras@apsega.lt>
2016-11-01 14:50:18 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,5 +9,7 @@ USERNAME=$(cat /root/.config/deluge/auth | awk -F: '{print $1}')
 sed "s@username_goes_here@$USERNAME@g" -i /root/.flexget/config.yml
 sed "s@password_goes_here@$PASSWORD@g" -i /root/.flexget/config.yml
 
+flexget daemon start &
+
 /usr/bin/deluge-web 
 
